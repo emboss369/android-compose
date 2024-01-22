ded7e95e895494c2656a9025a99fa58b07e2efc6
emboss369 <knd@hera.eonet.ne.jp>
2024-01-23 00:12:42 +0900

    Add slideshow images and implement ActionsRow and PagerItem

---
+++ 12_MySlideshow/Complete/MySlideshow/app/src/main/java/com/example/myslideshow/ActionsRow.kt
@@ -0,0 +1,19 @@
+package com.example.myslideshow
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myslideshow.ui.theme.MySlideshowTheme
+
+@Composable
+fun ActionRow(
+
+) {
+
+}
+@Preview
+@Composable
+fun ActionRowPreview() {
+  MySlideshowTheme {
+    ActionRow()
+  }
+}
---
+++ 12_MySlideshow/Complete/MySlideshow/app/src/main/java/com/example/myslideshow/TopBar.kt
@@ -0,0 +1,51 @@
+package com.example.myslideshow
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Slideshow
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myslideshow.ui.theme.MySlideshowTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+  TopAppBar(
+    title = {
+      Text(
+        text = "スライドショーアプリ",
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        color = MaterialTheme.colorScheme.onPrimary
+      )
+    },
+    navigationIcon = {
+      IconButton(onClick = { /* doSomething() */ }) {
+        Icon(
+          imageVector = Icons.Default.Slideshow,
+          tint = MaterialTheme.colorScheme.onPrimary,
+          contentDescription = "Slideshow"
+        )
+      }
+    },
+    colors = TopAppBarDefaults.topAppBarColors(
+      containerColor = MaterialTheme.colorScheme.primary
+    )
+  )
+}
+
+@Preview
+@Composable
+fun TopBarPrev() {
+  MySlideshowTheme {
+    TopBar()
+  }
+}
