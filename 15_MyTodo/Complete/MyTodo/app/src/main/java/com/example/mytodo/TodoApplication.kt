fa04a33656fe44dfde72c5c35acb5fb777ea8eb9
emboss369 <knd@hera.eonet.ne.jp>
2024-02-18 19:18:53 +0900

    TodoApplicationクラスの不要なコメントを削除

--- 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/TodoApplication.kt
+++ 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/TodoApplication.kt
@@ -4,20 +4,7 @@ import android.app.Application
 import com.example.mytodo.data.AppContainer
 import com.example.mytodo.data.AppDataContainer
 
-
-/**
- * Android Manifestに登録しておきます。
- *     <application android:name=".TodoApplication" を追加します。
- *
- * TodoApplicationはアプリ「全体」のことと捉えてください。
- * Androidアプリが起動した時に最初にインスタンスが作られるクラスです。
- * このクラスでは、アプリ全体で用いる設定をこのクラスで行います。
- * AndroidManifestに登録しないと、このクラスは生成されませんので注意が必要。
- * ここでは、AppDataMemoryContainerを生成して保持しています。
- */
 class TodoApplication : Application() {
-
-  // 他のクラスが依存関係を取得するために使用するAppContainerインスタンス。
   lateinit var container: AppContainer
   override fun onCreate() {
     super.onCreate()
