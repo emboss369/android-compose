3e091d884a7660d4e72d9b34e52719945abd52eb
emboss369 <knd@hera.eonet.ne.jp>
2024-02-18 19:22:00 +0900

    Fix AppViewModelProvider and add ItemEditViewModel

--- 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/ui/AppViewModelProvider.kt
+++ 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/ui/AppViewModelProvider.kt
@@ -10,46 +10,23 @@ import com.example.mytodo.ui.home.HomeViewModel
 import com.example.mytodo.ui.item.ItemEditViewModel
 import com.example.mytodo.ui.item.ItemEntryViewModel
 
-
-/**
- * Provides Factory to create instance of ViewModel for the entire Inventory app
- * Inventory アプリ全体の ViewModel インスタンスを作成するファクトリーを提供する。
- */
-
-// object はシングルトン
-// シングルトンは非常に便利なパターンであり、Kotlin（Scalaの後です）は、シングルトンを容易に宣言で切るようにしました
-// オブジェクト宣言の構文はシンプルです。クラス名の前にclassではなく「object」と付け加えるだけ。
-// これでインスタンスを1つしか持たない、シングルトンオブジェクトが定義できました。
-// 「AppViewModelProvider」はクラス名でもあり、ただ1つのインスタンス名でもあります。
 object AppViewModelProvider {
-
   val Factory = viewModelFactory {
-
     initializer {
-
-
+      HomeViewModel(todoApplication().container.itemsRepository)
+    }
+    initializer {
+      ItemEntryViewModel(todoApplication().container.itemsRepository)
+    }
+    initializer {
       ItemEditViewModel(
-        /**
-         ViewModelのデータを保存/復元するためSavedStateHandle の新しいインスタンスを作成するには、
-         [CreationExtras].createSavedStateHandle().createSavedStateHandle()
-         関数を使用して ViewModel に渡します。
-         */
         this.createSavedStateHandle(),
         todoApplication().container.itemsRepository
       )
     }
-
-    initializer {
-      ItemEntryViewModel(todoApplication().container.itemsRepository)
-    }
-
-    // Initializer for HomeViewModel
-    initializer {
-      HomeViewModel(todoApplication().container.itemsRepository)
-    }
   }
 }
 
-// CreationExtrasから、Applicationを取得できます。
 fun CreationExtras.todoApplication(): TodoApplication =
-  (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as TodoApplication)
+  (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
+  as TodoApplication)
