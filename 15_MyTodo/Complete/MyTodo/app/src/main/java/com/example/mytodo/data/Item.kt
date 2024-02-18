45bc8690f4ab598e752758b3c130f2a9609ff990
emboss369 <knd@hera.eonet.ne.jp>
2024-02-18 19:20:14 +0900

    Item.ktの変更: idプロパティに@PrimaryKeyアノテーションを追加

--- 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/data/Item.kt
+++ 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/data/Item.kt
@@ -2,15 +2,11 @@ package com.example.mytodo.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.concurrent.ThreadLocalRandom
+
 @Entity(tableName = "items")
 data class Item(
-  // id プロパティに @PrimaryKey アノテーションを付けて、id を主キーにします。主キーは、Item テーブルのすべてのレコードやエントリを一意に識別する ID です。
-  // id にデフォルト値として 0 を割り当てます。これは、id が id 値を自動生成するために必要です。
-  // Room が各エンティティの増分 ID を生成するように、パラメータ autoGenerate を true に設定します。このようにすることで、各アイテムの ID が一意になります。
-@PrimaryKey(autoGenerate = true)
+  @PrimaryKey(autoGenerate = true)
   val id: Int = 0,
-  //val id: Int = ThreadLocalRandom.current().nextInt(),
   val title: String,
   val description: String,
   val done: Boolean
