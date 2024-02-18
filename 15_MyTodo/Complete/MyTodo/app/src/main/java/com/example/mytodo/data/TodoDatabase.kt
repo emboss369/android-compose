af492c96eeecf130bddd9fa9672841d9473b02c1
emboss369 <knd@hera.eonet.ne.jp>
2024-02-18 19:21:24 +0900

    TodoDatabaseのデータベース名を"item_database"から"todo_database"に変更

--- 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/data/TodoDatabase.kt
+++ 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/data/TodoDatabase.kt
@@ -5,20 +5,18 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-
 @Database(entities = [Item::class], version = 1, exportSchema = false)
 abstract class TodoDatabase : RoomDatabase() {
   abstract fun itemDao(): ItemDao
 
   companion object {
-
     @Volatile
     private var Instance: TodoDatabase? = null
 
     fun getDatabase(context: Context): TodoDatabase {
       return Instance ?: synchronized(this) {
         Room.databaseBuilder(
-          context, TodoDatabase::class.java, "item_database"
+          context, TodoDatabase::class.java, "todo_database"
         ).build().also { Instance = it }
       }
     }
