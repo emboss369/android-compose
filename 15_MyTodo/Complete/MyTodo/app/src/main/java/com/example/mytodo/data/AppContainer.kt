fdd1448cd6f673481dd1d5efbe6dd7dabc2f4570
emboss369 <knd@hera.eonet.ne.jp>
2024-02-18 19:19:36 +0900

    AppContainerの実装を修正

--- 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/data/AppContainer.kt
+++ 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/data/AppContainer.kt
@@ -2,18 +2,12 @@ package com.example.mytodo.data
 
 import android.content.Context
 
-
 interface AppContainer {
   val itemsRepository: ItemsRepository
 }
 
-
 class AppDataContainer(private val context: Context) : AppContainer {
-  /**
-   * Implementation for [ItemsRepository]
-   */
   override val itemsRepository: ItemsRepository by lazy {
     DatabaseItemsRepository(TodoDatabase.getDatabase(context).itemDao())
   }
 }
-
