85efcc64a9c3f92a78247324a7d02f42d7664186
emboss369 <knd@hera.eonet.ne.jp>
2024-02-18 19:26:18 +0900

    Remove unused code in NavigationDestination

--- 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/ui/navigation/NavigationDestination.kt
+++ 15_MyTodo/Complete/MyTodo/app/src/main/java/com/example/mytodo/ui/navigation/NavigationDestination.kt
@@ -1,13 +1,6 @@
 package com.example.mytodo.ui.navigation
 
 interface NavigationDestination {
-  /**
-   * コンポーザブルなパスを定義するための一意な名前。
-   */
   val route: String
-
-  /**
-   * 画面に表示するタイトルを含む文字列リソースID。
-   */
   val titleRes: Int
 }
